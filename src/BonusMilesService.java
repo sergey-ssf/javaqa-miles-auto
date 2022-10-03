f92fbd6b06e472ebf5143c5dfe4780a6fca89ee1
Sergey Sakhanskiy <ssf808867@gmail.com>
2022-10-03 18:33:16 +0300

    Corrected commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class BonusMilesService {
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int miles = cost/20;
+        return miles;
+};
 }
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
+    }
+}
 
-        int pay = 5000;
-        int bonus = pay / 20;
-
-        System.out.println(bonus + " miles");
-
-        // Объявляете переменные для входных данных и
-        // параметров программы: одну для хранения
-        // стоимости билета, другую для хранения количества
-        // рублей для одной бонусной милли
-
-        // Рассчитываете количество бонусных милль, используя
-        // значения заведённых переменных. Ответ сохраняете в
-        // новую переменную и выводите на экран
-    }}
